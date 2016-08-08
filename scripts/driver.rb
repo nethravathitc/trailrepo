06a0fd2c6fbceb9690bc676f24038616f8391896
nethravathitc <nethravathitc6@gmail.com>
2016-08-08 18:26:16 +0530

    driver with while loop

--- scripts/driver.rb
+++ scripts/driver.rb
@@ -1,41 +1,50 @@
-class Driver
-	require './hybrid_framework/funtional_lib/readexceldata.rb'
-	require './hybrid_framework/scripts/business_action.rb'
+require  '/home/styletag/QAnew/hybrid_framework/scripts/business_action.rb'
+require '/home/styletag/QAnew/hybrid_framework/funtional_lib/readexceldata.rb'
+include Read
+require '/home/styletag/QAnew/hybrid_framework/funtional_lib/utils.rb'
+include Utils
 
+class Driver
+	
 	def initialize
+		super('/home/styletag/QAnew/hybrid_framework/test_case/test_suit.xls')
+		@obj_action = BusinessAction.new
 	end
 	
 	def test_execution
-		@obj_excel = Read.new('./hybrid_framework/test_case/test_suit.xls')
-		@obj_action = Business_action.new
-
-		for i in 1..1
-			test_suit= @obj_excel.dataread(i,0)
-			puts "Test Suit Name :" + test_suit
+		
+		#for i in 1..1
+		i=1
+		while dataread(i,0)!= nil
+			test_suit= dataread(i,0)
+			puts "\n\nTest Suit Name :" + test_suit
 			
-			test_case_id = @obj_excel.dataread(i,1)
+			test_case_id = dataread(i,1)
 			puts "Test Case ID :" + test_case_id.to_s
 
-			execution_flag = @obj_excel.dataread(i,2)
+			execution_flag = dataread(i,2)
 			puts "Execution Flag :" + execution_flag
 		
 			if execution_flag=="YES"
-
-				action= @obj_excel.dataread(i,3)
-				puts "Action Name :" + action
-				@obj_action.method_trail1
-
-				action= @obj_excel.dataread(i,4)
-				puts "Action Name :" + action
-				@obj_action.method_trail2
+				j=3
+				while dataread(i,j)!=nil
+					action= dataread(i,j)
+					puts "Action Name :" + action
+					@obj_action.send("#{action}")
+					j=j+1
+				end
 			else
 				puts "Execution Flag :" + execution_flag
 
 			end
+			i=i+1
 
 		end
 
 	end
 end
+
+launch_styletag
+
 obj_driver = Driver.new
 obj_driver.test_execution
