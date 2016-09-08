010b7fea9eaa56e9a578268eee8f15edbbf59dd2
nethravathitc <nethravathitc6@gmail.com>
2016-09-08 17:11:42 +0530

    Sep 8

--- scripts/business_action.rb
+++ scripts/business_action.rb
@@ -39,7 +39,7 @@ class BusinessAction
       		if login=="Login"
         		#puts login
         		#puts "#{i}"
-        		i=i+1 #to point to the index to the next row
+        		i=i+1 #to point to the index of the next row
         		break
       		end
     	end
@@ -63,6 +63,7 @@ class BusinessAction
 
 	# LOGOUT FUNCTIONALITY
   	def logout
+      sleep(1)
     	menu = account_member_name
    		$driver.action.move_to(menu).perform
    		sleep(1)
@@ -74,20 +75,29 @@ class BusinessAction
     
     	sleep(2)
     	puts "click c1..."
-    	best_seller.click
+    	women.click
     	wait_for_spinner
-	    puts "click first sale..."
-    	first_sale.click
-    	wait_for_spinner
+	    #puts "click first sale..." NO MORE SALE VIEW
+    	#first_sale.click
+    	#wait_for_spinner
     	sleep(3)
-    	first_product.click
-    	sleep(3)
-    	puts "click first product ..."
-    	wait_for_spinner
-	    puts @prod1 = productviewpage_productname.text.downcase
-    	sleep(3)
-   
-    	begin    
+
+    	
+      @flag_no_product=0 
+      i=1
+      begin
+        #first_product.click
+        #$driver.find_element(:css,"#product-container > div > ul > li.product-wrap:nth-child(#{i}) > div.products-wrapper .go-to-product").click
+        
+        #it will click only non-pinned product ie normal product
+        $driver.find_element(:css,"#product-container > div.ng-isolate-scope > product-list > ul > li:nth-child(#{i}) > div > div.product-image > a > img").click
+        sleep(3)
+    	  puts "click product no #{i} ..."
+    	  wait_for_spinner
+	      puts @prod1 = productviewpage_productname.text.downcase  # storing the product name by down casting it
+    	   sleep(3)
+         
+      	begin    
       		puts "select size add to cart ..."
       		size.click
       		sleep(3)
@@ -99,28 +109,122 @@ class BusinessAction
           		add_to_cart_button.click
         	rescue
           		puts "product got soldout (or) product not available"
+              @flag_no_product=1
+              i++  # to select the next product
           		wait_for_spinner
         	end 
-      	end
+      	 end
+      end while @flag_no_product==1 
   	end
 
-  	# CHECKING MINICART--CLICK MINICART--> CHECK PRODUCT ADDED IN CART PAGE
+  	# CHECKING MINICART--CLICK MINICART--> CHECK ADDED PRODUCT IN CART PAGE
   	def cart_check
     	puts "cart"
-    	sleep(2)
-	    mini_cart_text.click
-    	wait_for_spinner
-    	puts @prod2 = cartpage_productname.text.downcase
-    	if(@prod1 == @prod2)
-    		puts "product is added to the cart: Pass"
-    	else
-      		puts "product is not added to the cart: Fail"
-    	end
-    	wait_for_spinner
-    	puts "Proceed to checkoout ..." 
-    	proceed_to_checkout.click  
-    	wait_for_spinner
+      if @flag_no_product==0  
+    	 sleep(2)
+	     mini_cart_text.click
+    	 wait_for_spinner
+    	 puts @prod2 = cartpage_productname.text.downcase
+    	 if(@prod1 == @prod2)
+    	   	puts "product is added to the cart: PASS"
+    	 else
+      		puts "product is not added to the cart: FAIL" # cart is invalid ie when items in the cart are sold out 
+          puts "Removing sold out item present in the cart"
+          remove_sold_out.click
+
+    	 end
+    	 #wait_for_spinner
+    	 #puts "Proceed to checkoout ..." 
+    	 #proceed_to_checkout.click  
+    	 #wait_for_spinner
+      
+      else
+        print "product is not added to cart because of non availability"
+      end
+
   	end
+
+    def proceed_to_pay
+      sleep(1)
+      #mini_cart_text.click
+      #wait_for_spinner
+      sleep(1)
+      puts "Proceed to checkoout ..." 
+      proceed_to_checkout.click  
+      wait_for_spinner
+      puts "continue as logged in"
+      wait_for_spinner
+      continue.click
+      wait_for_spinner
+      puts "add new address"
+      # click add new address
+      add_address_checkout.click
+      wait_for_spinner
+
+      for i in 1..20
+        login=dataread(i,0)
+         if login=="Login"
+            i=i+1 #to point to the index of the next row
+            break
+          end
+      end
+      address_pincode.send_keys dataread(i,3)
+      wait_for_spinner
+      firstname.send_keys dataread(i,4)
+      address_phone.send_keys dataread(i,5)
+      address_address1.send_keys dataread(i,6)
+      address_landmark.send_keys dataread(i,7)
+      create_btn.click
+      wait_for_spinner
+
+      puts "selecting address"
+      select_address.click
+      wait_for_spinner
+      continue3.click
+      wait_for_spinner
+
+      puts "clicking pay button"
+      proceed_pay_btn.click
+      longwait
+    end
+
+    def payment_method_cod
+      puts "payment_method - COD"
+      cod_payment.click
+      wait_for_spinner
+      if codbutton.displayed?
+        puts "Order placing as COD"
+        codbutton.click
+      else
+        puts "Sorry, no COD available"
+      end
+      sleep(10)
+      #$driver.switch_to.alert.accept # for fire fox browser
+      sleep(250)
+      wait_for_spinner
+
+       puts $driver.find_element(:css,"#order-cancel > div > section > p:nth-child(2) > span").text
+      
+    end
+
+    #CLEARING CART BY CLICKING ON EMPTY_MY_CART
+    def clear_cart
+      sleep(1)
+
+      element = $driver.find_element(:css,"#mini-cart-wrap > li > div.minicart-inner-div > a:nth-child(3) > span") 
+      puts "moving cursor to minicart"
+      $driver.action.move_to(element).perform
+
+      sleep(2) 
+      begin
+         empty_my_cart = $driver.find_element(:css,"#minicart-bottom > p.pull-left > a")
+         sleep(1)
+         empty_my_cart.click
+         sleep(1)
+       rescue 
+         puts "cart is already empty"
+       end 
+    end
 
 end
 
